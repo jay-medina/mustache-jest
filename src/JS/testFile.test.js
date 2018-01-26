92475fe201829e2d148fb61d98e51145cba562e3
Jose Medina <josemedina921@gmail.com>
2018-01-25 19:47:35 -0800

    update config and tests

--- src/JS/testFile.test.js
+++ src/JS/testFile.test.js
@@ -3,12 +3,25 @@ const renderToDiv = require('./testFile');
 describe('renderToDiv', () => {
   let divElement;
 
-  beforeEach(() => {
-    divElement = document.createElement('div');
-    renderToDiv(divElement, { name: 'josie', shouldShow: true });
+  describe('when shouldShow is true', () => {
+    beforeEach(() => {
+      divElement = document.createElement('div');
+      renderToDiv(divElement, { name: 'josie', shouldShow: true });
+    });
+
+    it('contains the content from the compiled template', () => {
+      expect(divElement.innerHTML).toMatchSnapshot();
+    });
   });
 
-  it('contains the content from the compiled template', () => {
-    expect(divElement.innerHTML).toMatchSnapshot();
+  describe('when shouldShow is false', () => {
+    beforeEach(() => {
+      divElement = document.createElement('div');
+      renderToDiv(divElement, { name: 'josie', shouldShow: false });
+    });
+
+    it('contains the content from the compiled template', () => {
+      expect(divElement.innerHTML).toMatchSnapshot();
+    });
   });
 });
