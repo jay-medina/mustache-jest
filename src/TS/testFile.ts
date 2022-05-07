50d1faf45d1d2a3399ef5a7908138109dea12996
Ian Parkinson <ianparkinson@gmail.com>
2022-05-07 21:36:42 +0100

    Simplify tests
    Jest 28 now requires the jsdom environment to be installed separately
    (https://jestjs.io/docs/upgrading-to-jest28#jsdom) and I found I had to
    add /** @jest-environment jsdom */ at the start of the test files. It
    seems cleaner to simplify these tests so that they test the returned
    string directly, instead of storing it in the DOM.

--- src/TS/testFile.ts
+++
@@ -1,7 +0,0 @@
-import template from './testTemplate.html';
-
-export function renderToDiv(div: HTMLDivElement, data: object) {
-  const htmlCompiled = template(data);
-
-  div.innerHTML = htmlCompiled;
-}
